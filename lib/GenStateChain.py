5890c0c5bc6d86cd29f643a5f4fb3ae1410531d8
Ni Lab <niharvardlab@gmail.com>
2024-03-19 10:35:24 -0400

    update a bunch of new features

--- lib/GenStateChain.py
+++ lib/GenStateChain.py
@@ -6,6 +6,7 @@ from os import stat
 import qutip as qt
 import numpy as np
 from matplotlib import pyplot as plt
+from itertools import product
 
 N = []
 """ List of the number of states in each atom of the chain """
@@ -15,6 +16,8 @@ gs_list = []
 """ This variable is a ground state atom in each element. Note that the "up" state is a Rydberg atom, and "down" state is a ground state atom """
 number = (int,float,complex)
 """ Defines a number as an int, float or complex """
+n_atoms = 0
+""" The number of atoms in the chain """
 
 qt = qt
 """ The qt variable gives access to the qutip module as qt, if you'd like """
@@ -33,13 +36,14 @@ def set_n(n):
         None
 
     """
-    global N, id_list, gs_list
+    global N, id_list, gs_list, n_atoms
     N = n
     id_list = []
     gs_list = []
     for elem in n:
         id_list.append(qt.qeye(elem))
         gs_list.append(qt.basis(elem,0))
+    n_atoms = len(id_list)
 
 def create_state_list(site_idx, vals):
     """
@@ -109,7 +113,7 @@ class States:
 class Operators:
 
     @staticmethod
-    def identity():
+    def identity(bSingle = 0, idx = 0):
         """
         Returns the identity operator.
 
@@ -124,7 +128,10 @@ class Operators:
 
         """
         global id_list
-        return qt.tensor(*id_list)
+        if bSingle:
+            return id_list[idx]
+        else:
+            return qt.tensor(*id_list)
 
     @staticmethod
     def zero():
@@ -231,7 +238,7 @@ class Operators:
         return qt.tensor(*local_id_list)
 
     @staticmethod
-    def sigmax(idx, states, bUnitary = 0):
+    def sigmax(idx, states, bUnitary = 0, bSingle=0):
         """
         Returns the operator which is a spin 1/2 sigma_x on the list of idxs. The states to swap are specified in states.
 
@@ -239,6 +246,7 @@ class Operators:
             idx: A list of indices for where we want to have a spin 1/2 sigma_x operator
             states: A list of lists, where each member is the states to swap for that particular site.
             bUnitary: Set to 1 to get an identity on all other states of the affected idx. Otherwise, the operator will annihilate unused states.
+            bSingle: Returns a single operator for the first element in idx and the first set of states in states.
 
         Returns:
             The appropriate swap operator.
@@ -249,6 +257,8 @@ class Operators:
         """
         global id_list, N
         local_id_list = id_list.copy()
+        if bSingle:
+            idx = [idx[0]]
         for this_idx, elem in enumerate(idx):
             op1 = qt.basis(N[elem], states[this_idx][0]) * qt.basis(N[elem], states[this_idx][1]).dag()
             local_id_list[elem] = op1 + op1.dag()
@@ -256,10 +266,13 @@ class Operators:
                 for i in range(N[elem]):
                     if i not in states[this_idx]:
                         local_id_list[elem] = local_id_list[elem] + qt.basis(N[elem], i).proj()
-        return qt.tensor(*local_id_list)
+        if bSingle:
+            return local_id_list[idx[0]]
+        else:
+            return qt.tensor(*local_id_list)
 
     @staticmethod
-    def sigmay(idx, states, bUnitary = 0):
+    def sigmay(idx, states, bUnitary = 0, bSingle = 0):
         """
         Returns the operator which is a spin 1/2 sigma_x on the list of idxs. The states to swap are specified in states.
 
@@ -267,6 +280,7 @@ class Operators:
             idx: A list of indices for where we want to have a spin 1/2 sigma_x operator
             states: A list of lists, where each member is the states to swap for that particular site.
             bUnitary: Set to 1 to get an identity on all other states of the affected idx. Otherwise, the operator will annihilate unused states.
+            bSingle: Returns a single operator for the first element in idx and the first set of states in states.
 
         Returns:
             The appropriate swap operator.
@@ -277,6 +291,8 @@ class Operators:
         """
         global id_list, N
         local_id_list = id_list.copy()
+        if bSingle:
+            idx = [idx[0]]
         for this_idx, elem in enumerate(idx):
             op1 = 1j * qt.basis(N[elem], states[this_idx][0]) * qt.basis(N[elem], states[this_idx][1]).dag()
             local_id_list[elem] = op1 + op1.dag()
@@ -284,10 +300,13 @@ class Operators:
                 for i in range(N[elem]):
                     if i not in states[this_idx]:
                         local_id_list[elem] = local_id_list[elem] + qt.basis(N[elem], i).proj()
-        return qt.tensor(*local_id_list)
+        if bSingle:
+            return local_id_list[idx[0]]
+        else:
+            return qt.tensor(*local_id_list)
 
     @staticmethod
-    def sigmaz(idx, states, bUnitary = 0):
+    def sigmaz(idx, states, bUnitary = 0, bSingle = 0):
         """
         Returns the operator which is a spin 1/2 sigma_z on the list of idxs. The states to swap are specified in states.
 
@@ -295,6 +314,7 @@ class Operators:
             idx: A list of indices for where we want to have a spin 1/2 sigma_z operator
             states: A list of lists, where each member is the states to swap for that particular site.
             bUnitary: Set to 1 to get an identity on all other states of the affected idx. Otherwise, the operator will annihilate unused states.
+            bSingle: Returns a single operator for the first element in idx and the first set of states in states.
 
         Returns:
             The appropriate swap operator.
@@ -305,14 +325,19 @@ class Operators:
         """
         global id_list, N
         local_id_list = id_list.copy()
+        if bSingle:
+            idx = [idx[0]]
         for this_idx, elem in enumerate(idx):
-            op1 = qt.basis(N[elem], states[this_idx][0]).proj() + qt.basis(N[elem], states[this_idx][1]).proj()
+            op1 = qt.basis(N[elem], states[this_idx][0]).proj() - qt.basis(N[elem], states[this_idx][1]).proj()
             local_id_list[elem] = op1
             if bUnitary:
                 for i in range(N[elem]):
                     if i not in states[this_idx]:
                         local_id_list[elem] = local_id_list[elem] + qt.basis(N[elem], i).proj()
-        return qt.tensor(*local_id_list)
+        if bSingle:
+            return local_id_list[idx[0]]
+        else:
+            return qt.tensor(*local_id_list)
 
     @staticmethod
     def proj(idx_list):
@@ -335,6 +360,48 @@ class Operators:
             if elem != -1:
                 local_id_list[idx] = qt.basis(N[idx], elem).proj()
         return qt.tensor(*local_id_list)
+
+    @staticmethod
+    def projOnSite(idx, state):
+        """
+        Returns an on-site operator for a projector.
+
+        Args:
+            idx: Site to use for the projector.
+            state: State for the projector. 
+
+        Returns:
+            An appropriate projection operator.
+
+        Raises:
+            None
+
+        """
+        global N
+        return qt.basis(N[idx], state).proj()
+
+    @staticmethod
+    def jump(idx_list):
+        """
+        Returns the operator with a jump operator on each atom in the chain. A -1 indicates identity on that site.
+
+        Args:
+            idx_list: An index for each atom to be projected into. A -1 specifies identity on this site.
+
+        Returns:
+            The appropriate jump operator.
+
+        Raises:
+            None
+
+        """
+        global id_list, N
+        local_id_list = id_list.copy()
+        for idx, elem in enumerate(idx_list):
+            if elem != -1:
+                local_id_list[idx] = qt.basis(N[idx], elem[0]) * qt.basis(N[idx], elem[1]).dag()
+        return qt.tensor(*local_id_list)
+
 
     @staticmethod
     def PairInteraction(coords, interaction_coeff, states, atom_types = None, selection_idxs = None, powers = None):
@@ -407,3 +474,144 @@ class Operators:
                 this_proj_specifier[j] = states[j]
                 res = res + inter_strength * Operators.proj(this_proj_specifier)
         return res
+
+    @staticmethod
+    def cGate(bit_type, target_op):
+        """
+            If bit_type = -2 it is target. Otherwise it is state. If bit_type = -1, it is not involved
+        """
+        global id_list, N
+        if not isinstance(target_op, list):
+            target_op = [target_op]
+        control_list = id_list.copy()
+        op1 = id_list.copy()
+        for idx, elem in enumerate(bit_type):
+            if elem >= 0:
+                # control bit, controlling when projector into elem.
+                control_list[idx] = qt.basis(N[idx], elem).proj()
+                op1[idx] = qt.basis(N[idx], elem).proj()
+            elif elem <= -2:
+                op1[idx] = target_op[-elem - 2]
+        return qt.tensor(*op1) + qt.tensor(*id_list) - qt.tensor(*control_list)
+
+    @staticmethod
+    def specify(op_list):
+        """
+        Returns the operator with the operators specified on each atom in the chain.
+
+        Args:
+            op_list: A list corresponding to an operator on each site. A 1 indicates identity on that site, and a -1 is negative identity on that site.
+
+        Returns:
+            The appropriate operator.
+
+        Raises:
+            None
+
+        """
+        global id_list
+        local_id_list = id_list.copy()
+        for idx, elem in enumerate(op_list):
+            if elem == 1:
+                continue
+            elif elem == -1:
+                local_id_list[idx] = -local_id_list[idx]
+            else:
+                local_id_list[idx] = elem
+        return qt.tensor(*local_id_list)
+
+
+class Dynamics:
+    def Unitary(H, init_state, tend, n_points, expects = [], names = [], bPlot = False):
+        if isinstance(init_state, qt.qobj.Qobj):
+            init = init_state
+        else:
+            init = States.specify(init_state)
+        tlist = np.linspace(0, tend, n_points)
+        e_list = []
+        e_str = []
+        for idx, elem in enumerate(expects):
+            if isinstance(elem, list):
+                e_list.append(Operators.proj(elem))
+                if names == []:
+                    e_str.append(str(elem))
+                else:
+                    e_str.append(names[idx])
+            else:
+                e_list.append(elem)
+                if names == []:
+                    e_str.append('qutip operator')
+                else:
+                    e_str.append(names[idx])
+            
+        opts = qt.solver.Options(store_final_state=True)
+        res = qt.sesolve(H, init, tlist, e_list, options=opts)
+        if bPlot:
+            n_expects = len(e_list)
+            plt.figure()
+            for i in range(n_expects):
+                plt.plot(tlist, res.expect[i])
+            plt.xlabel('Time')
+            plt.ylabel('Value')
+            plt.legend(e_str)
+        return res
+
+    def MasterEq(H, init_state, tend, n_points, jump_ops = [], expects = [], names = [], bPlot = False):
+        if isinstance(init_state, qt.qobj.Qobj):
+            init = init_state
+        else:
+            init = States.specify(init_state)
+        tlist = np.linspace(0, tend, n_points)
+        e_list = []
+        e_str = []
+        for idx, elem in enumerate(expects):
+            if isinstance(elem, list):
+                e_list.append(Operators.proj(elem))
+                if names == []:
+                    e_str.append(str(elem))
+                else:
+                    e_str.append(names[idx])
+            else:
+                e_list.append(elem)
+                if names == []:
+                    e_str.append('qutip operator')
+                else:
+                    e_str.append(names[idx])
+            
+        opts = qt.solver.Options(store_final_state=True)
+        res = qt.mesolve(H, init, tlist, jump_ops, e_list, options=opts)
+        if bPlot:
+            n_expects = len(e_list)
+            plt.figure()
+            for i in range(n_expects):
+                plt.plot(tlist, res.expect[i])
+            plt.xlabel('Time')
+            plt.ylabel('Value')
+            plt.legend(e_str)
+        return res
+
+def CalculateFidelity(func, target_op, states = None):
+    global N
+    if states is None:
+        states = []
+        for elem in N:
+            states.append([i for i in range(elem)])
+    nstates_calculated = 1
+    for elem in states:
+        nstates_calculated = nstates_calculated * len(elem)
+    nstates = np.prod(np.array(N))
+    result_matrix = np.zeros((nstates, nstates), dtype=np.complex128)
+    ideal_result_matrix = np.zeros((nstates, nstates), dtype=np.complex128)
+    op_dims = [N for i in range(2)]
+    op_shape = (nstates, nstates)
+    idx = 0
+    for perm in product(*states):
+        init_state = States.specify(perm)
+        result_state = func(init_state)
+        result_matrix[:,idx] = np.squeeze(result_state.full())
+        ideal_result_state = target_op * init_state
+        ideal_result_matrix[:, idx] = np.squeeze(ideal_result_state.full())
+        idx = idx + 1
+    result_op = qt.Qobj(result_matrix, dims=op_dims, shape=op_shape)
+    ideal_result_op = qt.Qobj(ideal_result_matrix, dims=op_dims, shape=op_shape)
+    return (result_op.dag() * ideal_result_op).tr() / nstates_calculated, result_op, ideal_result_op
